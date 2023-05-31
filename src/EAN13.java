b86f606d94708c308ce671089fb992df4ad24e36
frankit82 <francesco.porcella@gmail.com>
2023-05-31 10:32:45 +0200

    Documentazione aggiornata con commenti

--- src/EAN13.java
+++ src/EAN13.java
@@ -2,29 +2,48 @@ public class EAN13 {
     
     private String codice;
 
+    /**
+     * file che effettua le verifiche del codice EAN
+     * 
+     * @author Domenico Lorubbio
+     * @version 1.8
+     * @since 31/05/2023
+     * /
+     
+     /* @param c serve per settare il codice EAN
+     */
    
     public void setCodice(String c){
         codice = c;
     }
+    
+    /**
+     * 
+     * @return il codice EAN
+     */
 
     public String getCodice(){
         return codice;
     }
 
+    /**
+     * 
+     * @return false perchè verifica la lunghezza del codice EAN
+     */
 
     public boolean verificaCodice(){
 
-        // Verifica se l'attributo codice è vuoto o nullo
+        // Verifica se l'attributo del codice è vuoto o nullo
         if (codice.equals(null) || codice == ""){
             throw new NullPointerException("Il codice EAN-13 risulta vuoto o nullo");
         }
 
-        // Verifica se la lunghezza dell'attributo codice è di 13
+        // Verifica se la lunghezza dell'attributo codice rispetta la lunghezza di 13
         if(codice.length() != 13) {
             return false;
         }
         
-        // Implementazione dell'algoritmo di calcolo della cifra di controllo
+        // Calcolo della cifra di controllo
         int somma = 0;
         int cifra;
         
@@ -41,7 +60,7 @@ public class EAN13 {
         // Calcola la cifra di controllo
         int cifraDiControllo = (10 - (somma % 10));
 
-        // Confronta la cifra di controllo con quella contenuta nel codice EAN13 (ultima cifra)
+        // Confronta la cifra di controllo con quella del codice EAN13
         return cifraDiControllo == Character.getNumericValue(codice.charAt(12)); 
     }
 
